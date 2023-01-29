7f1db05980b8afb31cb71d57634bf7c3256f7a55
TennYi <1124365598@qq.com>
2023-01-29 22:37:16 +0800

    proj1a: ArrayDeque finished

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,187 @@
+public class ArrayDeque<T> {
+    /**
+     * Similar to sentinel nodes.
+     * Note that the value corresponding to sentFrontIdx/senBackIdx
+     * is excluded from the valid items. Here we use a circular view
+     * on the array, using modular arithmetic
+     *
+     * sentFrontIdx can also be considered nextFirst
+     * sentBackIdx can also be considered nextLast
+     */
+    private int sentFrontIdx;
+    private int sentBackIdx;
+
+    /**
+     * invariant: size == (sentBackIdx - sentFrontIdx - 1) mod capacity
+     */
+    private int size;
+    /**
+     * The maximum size of the ArrayDeque. Capacity should be doubled
+     * every time it is equal to size. and It should divide 2 if size
+     * is less than capacity * 0.25 and capacity >= 16. We initialize
+     * capacity to be 8.
+     */
+    private int capacity;
+
+    private T[] arrayBase;
+
+    /**
+     * create an empty array
+     */
+    public ArrayDeque() {
+        sentFrontIdx = 0;
+        sentBackIdx = 1;
+        size = 0;
+        capacity = 8;
+        arrayBase = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Helper function to calculate the true index counting from
+     * given index using modular arithmetic. Note that count can
+     * be negative.
+     */
+    private int getCircularIndex(int start, int count) {
+        return ((start + count ) % capacity + capacity) % capacity;
+    }
+
+    /**
+     * Given the idx of conceptual ArrayDeque, return the true index of
+     * arrayBase.
+     * @param idx
+     * @return
+     */
+    private int getTrueIdx(int idx) {
+        return getCircularIndex(sentFrontIdx, idx + 1);
+    }
+
+    /**
+     * Double the capacity and update the arrayBase
+     * Note that sentFrontIdx and sentBackIdx should all be modified
+     */
+    private void doubleCapacity() {
+        T[] arrayDoubled = (T[]) new Object[2 * capacity];
+        for (int i = 0; i < size; i++) {
+            //Note that sentFrontIdx is and excluded idx
+            arrayDoubled[i] = arrayBase[getCircularIndex(sentFrontIdx, i + 1)];
+        }
+        arrayBase = arrayDoubled;
+        capacity = arrayDoubled.length;
+        sentBackIdx = size - 1;
+        sentFrontIdx = 0;
+    }
+
+    /**
+     * Halve the capacity. It works under the assumption that capacity >= 2 * size
+     * and size should not be 0
+     */
+    private void halveCapacity() {
+        T[] arrayHalved = (T[]) new Object[capacity / 2];
+        for (int i = 0; i < size; i++) {
+            arrayHalved[i] = arrayBase[getTrueIdx(i)];
+        }
+        arrayBase = arrayHalved;
+        capacity = arrayHalved.length;
+        sentBackIdx = size - 1;
+        sentFrontIdx = 0;
+    }
+
+
+    /**
+     *  Adds an item of type T to the front of the deque.
+     * @param item
+     */
+    public void addFirst(T item) {
+        if (size == capacity) {
+            doubleCapacity();
+        }
+        sentFrontIdx = getCircularIndex(sentFrontIdx, -1);
+        arrayBase[getCircularIndex(sentFrontIdx, 1)] = item;
+        size += 1;
+    }
+
+    /**
+     *  Adds an item of type T to the back of the deque.
+     * @param item
+     */
+    public void addLast(T item) {
+        if (size == capacity) {
+            doubleCapacity();
+        }
+        sentBackIdx = getCircularIndex(sentBackIdx, 1);
+        arrayBase[getCircularIndex(sentBackIdx, -1)] = item;
+        size += 1;
+    }
+
+    /**
+     * Returns true if deque is empty, false otherwise.
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the number of items in the deque.
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Prints the items in the deque from first to last, separated by a space.
+     */
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arrayBase[getTrueIdx(i)]);
+            System.out.print(" ");
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+     * @return
+     */
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+
+        if (size <= capacity / 4 && capacity >= 16) {
+            halveCapacity();
+        }
+
+        sentFrontIdx = getCircularIndex(sentFrontIdx, 1);
+        size -= 1;
+        return arrayBase[getCircularIndex(sentFrontIdx, -1)];
+    }
+
+    /**
+     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
+     * @return
+     */
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        if (size <= capacity / 4 && capacity >= 16) {
+            halveCapacity();
+        }
+        sentBackIdx = getCircularIndex(sentBackIdx, -1);
+        size -= 1;
+        return arrayBase[getCircularIndex(sentBackIdx, 1)];
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     * @param index
+     * @return
+     */
+    public T get(int index) {
+        return arrayBase[getTrueIdx(index)];
+    }
+
+}
---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,141 @@
+public class ArrayDequeTest {
+    /* Utility method for printing out empty checks. */
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Prints a nice message based on whether a test passed.
+     * The \n means newline. */
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    /**
+     * Adds a few things to the ArrayDeque, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     * <p>
+     * && is the "and" operation.
+     */
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+        ArrayDeque<String> arrayd1 = new ArrayDeque<String>();
+
+        boolean passed = checkEmpty(true, arrayd1.isEmpty());
+
+        arrayd1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        passed = checkSize(1, arrayd1.size()) && passed;
+        passed = checkEmpty(false, arrayd1.isEmpty()) && passed;
+
+        arrayd1.addLast("middle");
+        passed = checkSize(2, arrayd1.size()) && passed;
+
+        arrayd1.addLast("back");
+        passed = checkSize(3, arrayd1.size()) && passed;
+
+        System.out.println("Printing out deque: ");
+        arrayd1.printDeque();
+
+        printTestStatus(passed);
+    }
+
+    /**
+     * Adds an item, then removes an item, and ensures that dll is empty afterwards.
+     */
+    public static void addRemoveTest() {
+
+        System.out.println("Running add/remove test.");
+
+        ArrayDeque<Integer> arrayd1 = new ArrayDeque<Integer>();
+        // should be empty
+        boolean passed = checkEmpty(true, arrayd1.isEmpty());
+
+        arrayd1.addFirst(10);
+        // should not be empty
+        passed = checkEmpty(false, arrayd1.isEmpty()) && passed;
+
+        arrayd1.removeFirst();
+        // should be empty
+        passed = checkEmpty(true, arrayd1.isEmpty()) && passed;
+
+        printTestStatus(passed);
+    }
+
+    /**
+     * add some Integer items and check the whether the sequence is as expected
+     * then remove something and check the sequence
+     */
+    public static void addGetRemoveTest() {
+        System.out.println("Running add/get/remove test.");
+
+        ArrayDeque<Integer> arrayd1 = new ArrayDeque<Integer>();
+        Integer[] exp = new Integer[4];
+        for (int i = 0; i < 4; i++) {
+            exp[i] = i;
+        }
+        //test the addFirst
+        arrayd1.addFirst(1);
+        arrayd1.addFirst(0);
+        for (int i = 0; i < arrayd1.size(); i++) {
+            if (!arrayd1.get(i).equals(exp[i])) {
+                System.out.println("Test failed! Something wrong with addFirst\n");
+                return;
+            }
+        }
+
+        //test the addLast
+        arrayd1.addLast(2);
+        arrayd1.addLast(3);
+        for (int i = 2; i < arrayd1.size(); i++) {
+            if (!arrayd1.get(i).equals(exp[i])) {
+                System.out.println("Test failed! Something wrong with addLast\n");
+                return;
+            }
+        }
+
+        //test Print
+        System.out.println("Next line you should see 0 1 2 3");
+        arrayd1.printDeque();
+
+        //test the removeFirst
+        arrayd1.removeFirst();  //should be 1 2 3 now
+        if (!arrayd1.get(0).equals(1)) {
+            System.out.println("Test failed! Something wrong with removeFirst\n");
+            return;
+        }
+
+        //test the removeLast
+        arrayd1.removeLast();   //should be 1 2 now
+        if (!arrayd1.get(1).equals(2)) {
+            System.out.println("Test failed! Something wrong with removeLast\n");
+            return;
+        }
+        System.out.println("Test passed");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+        addIsEmptySizeTest();
+        addRemoveTest();
+        addGetRemoveTest();
+    }
+}
